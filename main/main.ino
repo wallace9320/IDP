cee88d3d34c884297605c40f35eb4e90244db7c0
Songha Kim <sk2161@cam.ac.uk>
2022-10-11 07:35:59 +0100

    created functions and classes

--- main/main.ino
+++ main/main.ino
@@ -7,8 +7,8 @@
 #define BLACK 1
 #define WHITE 0
 const int normalSpeed = 200;
-const int innerTurnSpeed = 100; //Speed of inner wheel when turning
-const int slowSpeed = 50; //Speed when very close to block
+const int innerTurnSpeed = 100; // Speed of inner wheel when turning
+const int slowSpeed = 50;       // Speed when very close to block
 const int blockDistance = 5;
 const int ceilingThreshold = 10;
 const int tunnelLeftClearance = 5;
@@ -30,10 +30,11 @@ bool drop = false;
 
 // motor set up
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor* ml = AFMS.getMotor(1);
-Adafruit_DCMotor* mr = AFMS.getMotor(2);
+Adafruit_DCMotor *ml = AFMS.getMotor(1);
+Adafruit_DCMotor *mr = AFMS.getMotor(2);
 
-void setup() {
+void setup()
+{
   // set the normal motor speed
   AFMS.begin();
   normalSpeed();
@@ -43,42 +44,50 @@ void setup() {
   rightMostLine.begin();
 }
 
-void loop() {
+void loop()
+{
   // if start is true run starting sequence else run main
   // frontUSQueue.push(frontUS)
-  if (start) initial(); //in initial, everything is hard coded until enter white line loop
-  else if (drop) drop(); //in drop, will do turning, dropping, backing, and change holding bool to false
-  else {
-    //if within 5 cm of block lower speed of motor
-    if (frontUS.reading(analogRead(FRONT_PIN)) < 5) lowerSpeed();
-    else normalSpeed();
+  if (start)
+    initial(); // in initial, everything is hard coded until enter white line loop
+  else if (drop)
+    drop(); // in drop, will do turning, dropping, backing, and change holding bool to false
+  else
+  {
+    // if within 5 cm of block lower speed of motor
+    if (frontUS.reading(analogRead(FRONT_PIN)) < 5)
+      lowerSpeed();
+    else
+      normalSpeed();
 
-    //if within 1 cm of block stop
-      //initialize pick up sequence
-      //set encoder reference point
-      //arrest others; run all in one go
-    if (frontUS.reading(analogRead(FRONT_PIN)) < 1 && !pickup) pickupAll();
+    // if within 1 cm of block stop
+    // initialize pick up sequence
+    // set encoder reference point
+    // arrest others; run all in one go
+    if (frontUS.reading(analogRead(FRONT_PIN)) < 1 && !pickup)
+      pickupAll();
 
-    //if in tunnel drive in tunnel else follow line
-    if (topIR.reading(analogRead(TOP_PIN)) < ceilingThreshold) tunnelDriving();
-    else followLine();
+    // if in tunnel drive in tunnel else follow line
+    if (topIR.reading(analogRead(TOP_PIN)) < ceilingThreshold)
+      tunnelDriving();
+    else
+      followLine();
 
-    //The first condition is only trigger if 3 seconds elapsed since last addition of white line, so same line won't be calculated twice
-    //The second conditionis if is to make sure it has traveled a certain distance, ie must be somewhere far away,
-    //so crossing that white cross won't trigger this
-    if (millis() - timeStart > 3000 && encoderReading > encoderThreshold && rightMostLine.reading(analogRead(RIGHT_MOST_LINE_PIN)) == WHITE) {
+    // The first condition is only trigger if 3 seconds elapsed since last addition of white line, so same line won't be calculated twice
+    // The second conditionis if is to make sure it has traveled a certain distance, ie must be somewhere far away,
+    // so crossing that white cross won't trigger this
+    if (millis() - timeStart > 3000 && encoderReading > encoderThreshold && rightMostLine.reading(analogRead(RIGHT_MOST_LINE_PIN)) == WHITE)
+    {
       noOfWhiteLines++;
-      //we can add an LED to signal this happened
+      // we can add an LED to signal this happened
       timeStart = millis();
-    }    
+    }
 
-    if (holding && (magnet && noOfWhiteLines >= 3 || !magnet && noOfWhiteLines == 1)) drop = true;
+    if (holding && (magnet && noOfWhiteLines >= 3 || !magnet && noOfWhiteLines == 1))
+      drop = true;
 
     // DO IT LATER
-    //if receive command from user
-      //begin parking sequence
-
+    // if receive command from user
+    // begin parking sequence
   }
-
 }
-
--- main/moving.ino
+++ main/moving.ino
@@ -1,3 +1,136 @@
+// Line follower
+// TODO - 1. read line sensor
+//      - 2. set motors based on line sensor readings
+
+setMotors robot; // created class
+
+// LS pins (analog input)
+const int leftLSPin = A0;
+const int rightLSPin = A1;
+
+// timing LS - better way than using delay function
+unsigned long lineSensorCm; // line sensor current millis
+unsigned long lineSensorPm; // line sensor previous millis
+const int LS_PERIOD = 50;   // checking line sensor value in every 50 ms
+
+// timing Motor
+unsigned long motorCm;
+unsigned long motorPm;
+const int MOTOR_PERIOD = 50; // updating motor speeds in every 50ms
+
+// debug purpose, when we change to true, it will print out values that sensor reads on serial monitor
+bool LSDebug = false;
+bool MotorDebug = false;
+
+// initialise the line sensor values
+int leftLSReading = 0;
+int rightLSReading = 0;
+const int LS_THRESHOLD = 500; // distinguish black and white colour
+
+// function for reading line sensor values and moving the robot
+void readLSAndMove()
+{
+  // to avoid using delay() -> reading line sensor values in every LS_PERIOD
+  lineSensorCm = millis();
+  if (lineSensorCm > lineSensorPm + LS_PERIOD)
+  {
+    // reading left and right LS
+    leftLSReading = analogRead(leftLSPin);
+    rightLSReading = analogRead(rightLSPin);
+
+    // only print out the values when we need to debug (to make serial monitor clean)
+    if (LSDebug)
+    {
+      Serial.print("leftLS: ");
+      Serial.print(leftLSReading);
+      Serial.print(", rightLS: ");
+      Serial.println(rightLSReading);
+    }
+
+    // reset previous millis
+    lineSensorPm = lineSensorCm;
+  }
+  // moving robot after reading line sensor values
+  // adjusting motor speed when the robot moves over the white line
+  // white: < threshold
+  // black: > threshold  * should as elec team about what values black and white colour get
+  // right = black and left = black
+  if (leftLSReading > LS_THRESHOLD && rightLSReading > LS_THRESHOLD)
+  {
+    // move forwards
+    robot.moveForward();
+  } // right = black and left = white
+  elif (leftLSReading > LS_THRESHOLD && rightLSReading < LS_THRESHOLD)
+  {
+    // turn left
+    robot.turnLeft();
+  } // right = white and left = black
+  elif (leftLSReading < LS_THRESHOLD && rightLSReading > LS_THRESHOLD)
+  {
+    // turn right
+    robot.turnRight();
+  }
+  else
+  {
+    // stop
+    robot.stop();
+  }
+}
+
+// setting motor speeds and direction
+// all the possible movement functions will place here
+class setMotors
+{
+
+private: // can only change this value inside this class, cannot be modified outside
+  float MOTOR_BASE_SPEED = 300.0;
+  float MOTOR_MIN_SPEED = 20.0; // when we move over the ramp, it needs to be greater than min speed
+                                // otherwise it will move backwards
+public:
+  float motorLeftSpeed;
+  float motorRightSpeed;
+
+  void moveForward()
+  { // update motor speed in every MOTOR_PERIOD
+    motorCm = millis();
+    if (motorCm > motorPm + MOTOR_PERIOD)
+    {
+      // will create motorLeft and motorRight object
+      motorLeft.setSpeed(MOTOR_BASE_SPEED);
+      motorRight.setSpeed(MOTOR_BASE_SPEED);
+
+      // debug
+      printSpeed();
+
+      // reset motor previous millis
+      motorPm = motorCm;
+    };
+  }
+  // will code later but similar to moveForward function above.
+  void turnRight(){
+      // check motor speed and wheels' movement and set appropriate speed values to turn robot in desired angle
+  };
+  void turnLeft();
+  void moveBackward();
+  void stop()
+  {
+    motorLeft.setSpeed(0);
+    motorRight.setSpeed(0);
+  }
+  // only print out left and right speed when MotorDebug = true.
+  void printSpeed()
+  {
+    if (MotorDebug)
+    {
+      Serial.print("Left speed: ");
+      Serial.print(motorLeftSpeed);
+      Serial.print(", Right speed: ");
+      Serial.println(motorRightSpeed);
+    }
+  }
+}
+
+/*
 void lowerSpeed() {
   ml->setSpeed(slowBlock);
   mr->setSpeed(slowBlock);
@@ -27,3 +160,5 @@ void tunnelDriving() {
   else if (leftUS > tunnelLeftClearance - 0.5) ml->setSpeed(innerTurnSpeed);
   else normalSpeed();
 }
+
+*/
