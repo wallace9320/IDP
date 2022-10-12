360a5e60b24694773869cb93a5ea8637300f5b2c
Songha Kim <sk2161@cam.ac.uk>
2022-10-12 09:42:25 +0100

    add button condition

--- main/main.ino
+++ main/main.ino
@@ -49,7 +49,8 @@ Adafruit_DCMotor *ml = AFMS.getMotor(1);
 Adafruit_DCMotor *mr = AFMS.getMotor(2);
 
 // process value from US sensor
-int readUSSensor(front=true) {
+int readUSSensor(front = true)
+{
   int trigPin = front ? frontUStrigPin : leftUStrigPin;
   int echoPin = front ? frontUSechoPin : leftUSechoPin;
   digitalWrite(trigPin, LOW);
@@ -73,7 +74,7 @@ void setup()
   rightMostLine.begin();
 
   // pin initialization
-  pinMode(leftLSPin, INPUT); 
+  pinMode(leftLSPin, INPUT);
   pinMode(rightLSPin, INPUT);
   pinMode(farRightLSPin, INPUT);
   pinMode(frontUStrigPin, OUTPUT);
@@ -84,48 +85,51 @@ void setup()
 
 void loop()
 {
-  // if start is true run starting sequence else run main
-  // frontUSQueue.push(frontUS)
-  if (start)
-    initial(); // in initial, everything is hard coded until enter white line loop
-  else if (drop)
-    drop(); // in drop, will do turning, dropping, backing, and change holding bool to false
-  else
+  if (button)
   {
-    // if within 5 cm of block lower speed of motor
-    if (frontUS.reading(readUSSensor(front=true)) < 5)
-      lowerSpeed();
+    // if start is true run starting sequence else run main
+    // frontUSQueue.push(frontUS)
+    if (start)
+      initial(); // in initial, everything is hard coded until enter white line loop
+    else if (drop)
+      drop(); // in drop, will do turning, dropping, backing, and change holding bool to false
     else
-      normalSpeed();
-
-    // if within 1 cm of block stop
-    // initialize pick up sequence
-    // set encoder reference point
-    // arrest others; run all in one go
-    if (frontUS.reading(readUSSensor(front=true)) < 1 && !pickup)
-      pickupAll();
-
-    // if in tunnel drive in tunnel else follow line
-    if (topIR.reading(topIRSensor.distance()) < ceilingThreshold)
-      tunnelDriving();
-    else
-      followLine();
-
-    // The first condition is only trigger if 3 seconds elapsed since last addition of white line, so same line won't be calculated twice
-    // The second conditionis if is to make sure it has traveled a certain distance, ie must be somewhere far away,
-    // so crossing that white cross won't trigger this
-    if (millis() - timeStart > 3000 && encoderReading > encoderThreshold && rightMostLine.reading(analogRead(RIGHT_MOST_LINE_PIN)) == WHITE)
     {
-      noOfWhiteLines++;
-      // we can add an LED to signal this happened
-      timeStart = millis();
+      // if within 5 cm of block lower speed of motor
+      if (frontUS.reading(readUSSensor(front = true)) < 5)
+        lowerSpeed();
+      else
+        normalSpeed();
+
+      // if within 1 cm of block stop
+      // initialize pick up sequence
+      // set encoder reference point
+      // arrest others; run all in one go
+      if (frontUS.reading(readUSSensor(front = true)) < 1 && !pickup)
+        pickupAll();
+
+      // if in tunnel drive in tunnel else follow line
+      if (topIR.reading(topIRSensor.distance()) < ceilingThreshold)
+        tunnelDriving();
+      else
+        followLine();
+
+      // The first condition is only trigger if 3 seconds elapsed since last addition of white line, so same line won't be calculated twice
+      // The second conditionis if is to make sure it has traveled a certain distance, ie must be somewhere far away,
+      // so crossing that white cross won't trigger this
+      if (millis() - timeStart > 3000 && encoderReading > encoderThreshold && rightMostLine.reading(analogRead(RIGHT_MOST_LINE_PIN)) == WHITE)
+      {
+        noOfWhiteLines++;
+        // we can add an LED to signal this happened
+        timeStart = millis();
+      }
+
+      if (holding && (magnet && noOfWhiteLines >= 3 || !magnet && noOfWhiteLines == 1))
+        drop = true;
+
+      // DO IT LATER
+      // if receive command from user
+      // begin parking sequence
     }
-
-    if (holding && (magnet && noOfWhiteLines >= 3 || !magnet && noOfWhiteLines == 1))
-      drop = true;
-
-    // DO IT LATER
-    // if receive command from user
-    // begin parking sequence
   }
 }
