d20542f0e1d557879bd546a4fe0736c88e348abb
Songha Kim <sk2161@cam.ac.uk>
2022-10-13 09:04:10 +0100

    change function name

--- main/initial.ino
+++ main/initial.ino
@@ -1,6 +1,6 @@
 // move forward 20 cm and turn right
 // time estimated for now, need to test and put the right time values
-void initial()
+void initialMovement()
 {
     if (millis() - buttonTime < 3000)
     {
