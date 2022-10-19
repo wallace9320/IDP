207e090cf3b54a924ac9cea6ee7475c53a690a46
Wallace Leung <chaksumleung@gmail.com>
2022-10-19 15:16:30 +0100

    motor

--- ref/motor/motor.ino
+++ ref/motor/motor.ino
@@ -9,39 +9,55 @@ Adafruit_DCMotor* mr = AFMS.getMotor(2);
 #define BLACK 0
 #define WHITE 1
 unsigned long timeStart;
-const int normalSpeed = 150;
-const int ot = 120;
-const int it = 30;
+const int normalSpeed = 255;
+const int ot = 200;
+const int it = 0;
 int leftLSReading = 0;
 int rightLSReading = 0;
+int rrrReading = 0;
 const int leftLSPin = 0;
-const int rightLSPin = 1;
-const int rrrPin = 2;
+const int rightLSPin = 2;
+const int rrrPin = 1;
 int mlSpeed = normalSpeed;
-int mrSpeed = noramlSpeed;
+int mrSpeed = normalSpeed;
 
 void setNormalSpeed() {
   ml->setSpeed(normalSpeed);
   mr->setSpeed(normalSpeed);
+  mlSpeed = normalSpeed;
+  mrSpeed = normalSpeed;
 }
 
 void followLine() {
   leftLSReading = digitalRead(leftLSPin);
   rightLSReading = digitalRead(rightLSPin);
   rrrReading = digitalRead(rrrPin);
-  Serial.print(leftLSReading);
-  Serial.print(" <-L  R-> ");
-  Serial.println(rightLSReading);
+  // Serial.print(leftLSReading);
+  // Serial.print(" <-L  R-> ");
+  // Serial.print(rightLSReading);
+  // Serial.print("       RM-> ");
+  // Serial.println(rrrReading);
+  if (millis() - timeStart > 500) {
+    timeStart = millis();
+    Serial.print(mlSpeed);
+    Serial.print(" <-L  R-> ");
+    Serial.println(mrSpeed);
+  }
+
   if (leftLSReading == WHITE && rightLSReading == BLACK) {
     if (mlSpeed != ot || mrSpeed != it) {
       ml->setSpeed(ot);
       mr->setSpeed(it);
+      mlSpeed = ot;
+      mrSpeed = it;
     }
   }
   else if (leftLSReading == BLACK && rightLSReading == WHITE && rrrReading == BLACK) {
     if (mlSpeed != it || mrSpeed != ot) {
       ml->setSpeed(it);
       mr->setSpeed(ot);
+      mlSpeed = it;
+      mrSpeed = ot;
     }
   }  
   else if (mlSpeed != normalSpeed || mrSpeed != normalSpeed) {
@@ -65,7 +81,6 @@ void setup() {
 void loop() {
 
   followLine();
-
   // mr->setSpeed(innerTurnSpeed);
   // mr->run(BACKWARD);
   // delay(1000);
@@ -87,6 +102,5 @@ void loop() {
   //   m1->setSpeed(100);
   //   m2->setSpeed(100);
   // }
-
 
 }
